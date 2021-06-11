fbad1b3c4fa330290a139b9010077b10841bbd86
Dhaya94 <dhayananth94@gmail.com>
2021-06-11 20:09:10 +1000

    feat: Added the feature to allow a cell to be coloured

--- designs.js
+++ designs.js
@@ -1,30 +1,44 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 const table = document.getElementById('pixelCanvas');
 
-
-function makeGrid() { 
+//Function used to create the grid based on the user input
+function makeGrid(){ 
     table.innerHTML = '';
-    let tableRow = document.getElementById('inputHeight').valueAsNumber;
+    let tableRow = document.getElementById('inputHeight').valueAsNumber;    // Select size input
     let tableColumn = document.getElementById('inputWidth').valueAsNumber;   
     for (let i = 0; i < tableRow; i++){
         let row = table.insertRow();
         for (let j = 0; j < tableColumn; j++){
-            let tableCell = row.insertCell();
+            row.insertCell();                   
             }        
         }           
     }
 
-let submitButton = document.getElementsByTagName('input')[2];
+// Function used to set the background color of the cell selected
+function colorSetter(cell){
+    let color = document.getElementById('colorPicker').value;    // Select color input
+    cell.style.background = color; 
+}
 
-submitButton.addEventListener('click', function (e) {    
-    debugger;
+const submitButton = document.getElementsByTagName('input')[2];
+
+// When the user hits the submit button, calling the makeGrid() function
+submitButton.addEventListener('click', function (e) {       
     e.preventDefault();  
     makeGrid();
 });
+
+table.addEventListener('click', function(evt){ 
+    let cellClicked = evt.target;
+    if(cellClicked.nodeName === 'TD'){
+        colorSetter(cellClicked);  
+    }       
+});
+
+
+
+
+
+
 
 
 
--- test.js
+++
@@ -1,9 +0,0 @@
-var submitButton = document.getElementsByTagName('input')[2];
-var table = document.getElementById('pixelCanvas');
-
-submitButton.addEventListener('click', function(){
-    var tableRow = table.insertRow(0);
-    var tableCell = tableRow.insertCell(0);
- });
-
-

6baf8f10e433b9e27da4cd2dacfb8f16696088cd
Dhaya94 <dhayananth94@gmail.com>
2021-06-11 21:19:28 +1000

    style: Removed additional whitespace and fixed any identation errors

--- designs.js
+++ designs.js
@@ -1,37 +1,38 @@
 const table = document.getElementById('pixelCanvas');
 
-//Function used to create the grid based on the user input
-function makeGrid(){ 
+// Function used to create the grid based on the user input
+function makeGrid(){
     table.innerHTML = '';
-    let tableRow = document.getElementById('inputHeight').valueAsNumber;    // Select size input
-    let tableColumn = document.getElementById('inputWidth').valueAsNumber;   
+    const tableRow = document.getElementById('inputHeight').valueAsNumber;
+    const tableColumn = document.getElementById('inputWidth').valueAsNumber;
     for (let i = 0; i < tableRow; i++){
         let row = table.insertRow();
         for (let j = 0; j < tableColumn; j++){
-            row.insertCell();                   
+            row.insertCell();                
             }        
         }           
     }
 
 // Function used to set the background color of the cell selected
 function colorSetter(cell){
-    let color = document.getElementById('colorPicker').value;    // Select color input
-    cell.style.background = color; 
+    const color = document.getElementById('colorPicker').value;
+    cell.style.background = color;
 }
 
 const submitButton = document.getElementsByTagName('input')[2];
 
-// When the user hits the submit button, calling the makeGrid() function
+// Calling the makeGrid() function after the submit button is clicked
 submitButton.addEventListener('click', function (e) {       
-    e.preventDefault();  
+    e.preventDefault();
     makeGrid();
 });
 
-table.addEventListener('click', function(evt){ 
-    let cellClicked = evt.target;
+// Calling the color setter function after a cell in the table is selected
+table.addEventListener('click', function(evt){
+    const cellClicked = evt.target;
     if(cellClicked.nodeName === 'TD'){
-        colorSetter(cellClicked);  
-    }       
+        colorSetter(cellClicked);
+    }
 });
 
 

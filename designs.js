a5ee570ddfd2c03240c0e89322c8f26b4046f8e0
Dhaya94 <dhayananth94@gmail.com>
2021-06-11 18:54:55 +1000

    feat: Display grid based on the user input

--- designs.js
+++ designs.js
@@ -3,8 +3,28 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+const table = document.getElementById('pixelCanvas');
 
-// Your code goes here!
 
-}
+function makeGrid() { 
+    table.innerHTML = '';
+    let tableRow = document.getElementById('inputHeight').valueAsNumber;
+    let tableColumn = document.getElementById('inputWidth').valueAsNumber;   
+    for (let i = 0; i < tableRow; i++){
+        let row = table.insertRow();
+        for (let j = 0; j < tableColumn; j++){
+            let tableCell = row.insertCell();
+            }        
+        }           
+    }
+
+let submitButton = document.getElementsByTagName('input')[2];
+
+submitButton.addEventListener('click', function (e) {    
+    debugger;
+    e.preventDefault();  
+    makeGrid();
+});
+
+
+
---
+++ test.js
@@ -0,0 +1,9 @@
+var submitButton = document.getElementsByTagName('input')[2];
+var table = document.getElementById('pixelCanvas');
+
+submitButton.addEventListener('click', function(){
+    var tableRow = table.insertRow(0);
+    var tableCell = tableRow.insertCell(0);
+ });
+
+
